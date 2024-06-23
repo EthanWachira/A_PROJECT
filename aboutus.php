4f60ad863b5bb2257618d084f7abb695156c886f
EthanWachira <166112805+EthanWachira@users.noreply.github.com>
2024-06-23 16:54:35 +0300

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -40,25 +40,21 @@
             </div>
         </nav>
     </header>
+    
     <div class="container mt-4">
+        <h2>About Us</h2>
+        <p>This website is owned and created by Ethan Ng'ang'a Wachira. Ethan created this website to provide a resource that allows one to express how they feel, which he saw fit since speaking your mind or rather writing your thoughts down has various benefits, most of all bettering your mental health.</p>
+        <p>Ethan is currently pursuing a degree in Bachelors of Business and Information Technology at Strathmore University. He aims at enhancing the website even more and making sure it reaches the right audience.</p>
+    </div>
 
-<?php
-$pageTitle = "Digital Diary - About Us";
-?>
+    <footer class="mt-4">
+        <p>&copy; <?php echo date('Y'); ?> Digital Diary. All rights reserved.</p>
+    </footer>
 
-<div class="container mt-4">
-    <h2>About Us</h2>
-    <p>This website is owned and created by Ethan Ng'ang'a Wachira. Ethan created this website to provide a resource that allows one to express how they feel, which he saw fit since speaking your mind or rather writing your thoughts down has various benefits, most of all bettering your mental health.</p>
-    <p>Ethan is currently pursuing a degree in Bachelors of Business and Information Technology at Strathmore University. He aims at enhancing the website even more and making sure it reaches the right audience.</p>
-</div>
-
-<footer class="mt-4">
-    <p>&copy; <?php echo date('Y'); ?> Digital Diary. All rights reserved.</p>
-</footer>
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
+
 
