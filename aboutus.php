8c13ca6bb6b57239c9c382a7ee45bca71917cd0b
EthanWachira <166112805+EthanWachira@users.noreply.github.com>
2024-06-23 17:27:12 +0300

    Add files via upload

--- aboutus.php
+++ aboutus.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Digital Diary - Contact Us</title>
+    <title>Digital Diary - About Us</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <?php include 'header.php'; ?>
 
     <div class="container mt-4">
-        <h2>Contact Us</h2>
-        <p>Email: ethannganga495@gmail.com</p>
-        <p>Phone: +2540740492256</p>
+        <h2>About Us</h2>
+        <p>This website is owned and created by Ethan Ng'ang'a Wachira. Ethan created this website to provide a resource that allows one to express how they feel, which he saw fit since speaking your mind or rather writing your thoughts down has various benefits, most of all bettering your mental health.</p>
+        <p>Ethan is currently pursuing a degree in Bachelors of Business and Information Technology at Strathmore University. He aims at enhancing the website even more and making sure it reaches the right audience.</p>
     </div>
 
     <?php include 'footer.php'; ?>
