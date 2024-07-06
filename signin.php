48c53b1e0a96bf9188cdcacd545053ad3c7a9943
Ethan Wachira <ethan.wachira@strathmore.edu>
2024-07-06 14:02:55 +0300

    Initial commit

--- signin.php
+++ signin.php
@@ -3,17 +3,19 @@ $pageTitle = "Sign In";
 require_once 'header.php';
 ?>
 
-<form id="signinForm" class="container mt-4">
+<main class="container mt-4">
     <h2>Sign In</h2>
-    <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" class="form-control" id="email" name="email" required>
-    </div>
-    <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" class="form-control" id="password" name="password" required>
-    </div>
-    <button type="submit" class="btn btn-primary">Sign In</button>
-</form>
+    <form id="signinForm">
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Sign In</button>
+    </form>
+</main>
 
 <?php require_once 'footer.php'; ?>
--- signup.php
+++ signup.php
@@ -3,21 +3,23 @@ $pageTitle = "Sign Up";
 require_once 'header.php';
 ?>
 
-<form id="signupForm" class="container mt-4">
+<main class="container mt-4">
     <h2>Sign Up</h2>
-    <div class="form-group">
-        <label for="name">Name</label>
-        <input type="text" class="form-control" id="name" name="name" required>
-    </div>
-    <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" class="form-control" id="email" name="email" required>
-    </div>
-    <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" class="form-control" id="password" name="password" required>
-    </div>
-    <button type="submit" class="btn btn-primary">Sign Up</button>
-</form>
+    <form id="signupForm">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Sign Up</button>
+    </form>
+</main>
 
 <?php require_once 'footer.php'; ?>
